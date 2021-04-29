6e079742fa15e31caf97e574d8a21a80196b141a
Pranay <pranay.das0909@gmail.com>
2021-04-29 23:09:54 +0530

    Delete files

--- hashtag/db/query/admin.sql
+++
@@ -1,5 +0,0 @@
--- name: CreateAdmin :one
-INSERT INTO admin (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email;
-
--- name: Login :one
-SELECT id, name, email, password FROM admin WHERE email = ($1);
