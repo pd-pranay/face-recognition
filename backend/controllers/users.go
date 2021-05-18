f34dd3a40f2123a43e10560cdb39ed9b381c5fbc
Pranay <pranay.das0909@gmail.com>
2021-05-18 14:55:05 +0530

    Update controller

--- backend/controllers/users.go
+++ backend/controllers/users.go
@@ -94,7 +94,6 @@ func (uc *UsersController) CreateUser(c *fiber.Ctx) error {
 			"code": 200,
 			"data": users,
 		})
-
 }
 
 func (uc *UsersController) ReadAllUsers(c *fiber.Ctx) error {
@@ -137,19 +136,67 @@ func (uc *UsersController) ReadUserByID(c *fiber.Ctx) error {
 }
 
 func (uc *UsersController) UpdateUserByID(c *fiber.Ctx) error {
-	// user, err := uc.Queries.UpdateUser(c.Context(), c.Params("id"))
-	// if err != nil {
-	// 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-	// 		"code":  500,
-	// 		"error": err.Error(),
-	// 	})
-	// }
-	// return c.Status(fiber.StatusOK).
-	// 	JSON(fiber.Map{
-	// 		"code": 200,
-	// 		"data": user,
-	// 	})
-	return nil
+	id := c.Params("id")
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"code":  500,
+			"error": err.Error(),
+		})
+	}
+
+	fileType := strings.Split(file.Filename, ".")[1]
+
+	if isAllowedExt := allowedFileType(fileType); !isAllowedExt {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"code":  500,
+			"error": "File extension no allowed",
+		})
+	}
+
+	uid, err := uuid.NewUUID()
+	if err != nil {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"code":  500,
+			"error": err.Error(),
+		})
+	}
+
+	file.Filename = uid.String() + "." + fileType
+
+	imgPath := fmt.Sprintf("../ml/api/images/images_test/%s", file.Filename)
+	if err := c.SaveFile(file, imgPath); err != nil {
+		log.Println("err ", err)
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"code":  500,
+			"error": err.Error(),
+		})
+	}
+
+	body := db.UpdateUserParams{
+		ImageUid:    id,
+		ImageUid_2:  uid.String(),
+		Name:        sql.NullString{String: c.FormValue("name"), Valid: true},
+		CollegeName: sql.NullString{String: c.FormValue("college_name"), Valid: true},
+		Address:     sql.NullString{String: c.FormValue("address"), Valid: true},
+		MobileNo:    sql.NullInt32{Int32: 78, Valid: true},
+		ImagePath:   file.Filename,
+	}
+
+	users, err := uc.Queries.UpdateUser(c.Context(), body)
+	if err != nil {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"code":  500,
+			"error": err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).
+		JSON(fiber.Map{
+			"code": 200,
+			"data": users,
+		})
 }
 
 func (uc *UsersController) DeleteUserByID(c *fiber.Ctx) error {
